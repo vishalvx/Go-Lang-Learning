1a6fe1adb8319c6f526c015e03a368ffe85549db
vishalvx <dev.vishalprajapati.24@gmail.com>
2024-01-10 23:49:15 +0530

    remaining loops in go lang.

--- 11ConditionLoops/main.go
+++ 11ConditionLoops/main.go
@@ -40,17 +40,56 @@ func main() {
 		fmt.Println("you rolled 4 now you can move 4 steps")
 	case 5:
 		fmt.Println("you rolled 5 now you can move 5 steps")
+		/*the fallthrough statement is used within a switch statement to transfer control to the next case, even if the expression associated with that case does not match the value being switched on. The fallthrough statement is not used in the typical sequential manner as in some other programming languages.*/
+		fallthrough
 	case 6:
 		fmt.Println("you rolled 6 now you can move 6 steps")
 	default:
 		fmt.Println("WOW !! you rolled a number other than 1 to 6")
 	}
 
+	// for loop
 	fmt.Println("FOR LOOP")
 
-	// for loop
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
+	days := []string{"sunday", "tuesday", "wednesday", "thursday", "friday", "saturday"}
+	// for i := 0; i < len(days); i++ {
+	// 	fmt.Println("Day: ", days[i])
+	// }
+
+	// for index := range days {
+	// 	fmt.Println("Day: ", days[index])
+	// }
+
+	for _i, day := range days {
+		fmt.Printf("Index of day is %v and value is %v\n", _i, day)
 	}
 
+	randomvalue := 1
+
+	// Look like while loop
+	for randomvalue <= 10 {
+
+		if randomvalue == 7 {
+			fmt.Println("Thala for Reason", randomvalue)
+			break
+		}
+		// uncomment the below line to see the goto example
+		// if randomvalue == 5 {
+		// 	goto myLabel
+		// }
+
+		if randomvalue == 3 {
+			randomvalue++
+			continue
+		}
+
+		fmt.Println("Random Value: ", randomvalue)
+		randomvalue++
+	}
+
+	//	// create goto label
+	//
+	// myLabel:
+	//
+	//	fmt.Println("I am in the main function")
 }
